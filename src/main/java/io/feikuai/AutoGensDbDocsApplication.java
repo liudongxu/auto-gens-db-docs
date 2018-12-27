f9d4aef3da2dd6217ba25f899f501f9efc71d560
liudongxu <562430258@qq.com>
2018-12-27 12:26:02 +0800

    spring boot 价格搭建

---
+++ src/main/java/io/feikuai/AutoGensDbDocsApplication.java
@@ -0,0 +1,14 @@
+package io.feikuai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoGensDbDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoGensDbDocsApplication.class, args);
+	}
+
+}
+
